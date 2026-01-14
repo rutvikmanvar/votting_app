d670c54ed0441e07b03fd9c7aa913ead4c01be61
Rutvik Manvar <rutvikmanvar99@gmail.com>
2026-01-14 20:30:02 +0530

    add services file

---
+++ services/authService.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+const secret = process.env.SECRET;
+
+function setUser(user){
+    return jwt.sign({
+        id:user,
+    },
+    secret
+    )
+}
+
+function getUser(req,res,next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader) return res.json({success:false,message:'Unauthorized'});
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const user = jwt.verify(token,process.env.SECRET);
+        console.log(`user = ${JSON.stringify(user)}`)
+        req.user = user;
+        next();
+    } catch (error) {
+        return res.json({success:false,message:error.message})
+    }
+}
+
+module.exports = {
+    setUser,
+    getUser
+}
